198ef78d7f2be4126ca870534ef41392c7bb3e74
Anuj70007 <anujchaudhary70007@gmail.com>
2024-08-28 19:14:57 +0530

    fifth

---
+++ problem5/infinte.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    const char *files[5] = {"file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt"};
+    int fds[5]; 
+
+    while (1) {
+        for (int i = 0; i < 5; i++) {
+            fds[i] = open(files[i], O_RDWR | O_CREAT | O_TRUNC, 0666);
+            if (fds[i] == -1) {
+                perror("Error");
+                exit(1);
+            }
+            printf("File name:- %s file descriptor:- %d\n", files[i], fds[i]);
+        }
+        
+        sleep(10);
+        for (int i = 0; i < 5; i++) {
+            close(fds[i]);
+        }
+
+        sleep(1); 
+    }
+
+    return 0;
+}
+
