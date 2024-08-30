4251a1b6cfcdd6a26f010677f53cae083785c61a
Anuj70007 <anujchaudhary70007@gmail.com>
2024-08-30 15:30:34 +0530

    26

---
+++ problem26/26a.c
@@ -0,0 +1,21 @@
+/*
+============================================================================
+Name : 26a.c
+Author : Anuj Chaudhary
+Description : Write a program to execute an executable program.
+a. use some executable program
+b. pass some input to an executable program. (for example execute an executable of $./a.out name)
+Date: 30th Aug, 2024.
+============================================================================
+*/
+#include <stdio.h>
+
+int main(int count, char *in[]) {
+    if (count > 1) {
+        printf("my , %s!\n", in[1]);
+    } else {
+        printf("new\n");
+    }
+    return 0;
+}
+
